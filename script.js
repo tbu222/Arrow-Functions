fd35f554aad986f1d9658857c9b9531ed69367a8
tbu222 <thinhbui2911@gmail.com>
2022-11-03 08:12:09 -0700

    init

---
+++ script.js
@@ -0,0 +1,7 @@
+//ES2015 Arrow Functions Shorthand
+const double = arr => arr.map(val => val * 2);
+
+//Refactor the following function to use arrow functions:
+
+
+const squareAndFindEvens = numbers => numbers.map(val => val **2).filter(square => square %2 === 0)
